6095f8af903831ba40b6bb54130a04e6c915d049
Rodrigo Mejia Galea <00037119@uca.edu.sv>
2021-07-06 16:00:49 -0600

    Another fix

--- api/models/user.js
+++ api/models/user.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
     username: {type: String, required: true, unique:true},
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    phone: {type: String, required: true},
 
 });
 
